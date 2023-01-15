329f74c1e4cd0c6717bf90fa9ba4fd257da06f1e
patizani <pzanirati@gmail.com>
2023-01-15 17:27:04 -0300

    add video

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const api = axios.create({
-    // baseURL:"https://foodexplorer-api-g42c.onrender.com"
-    baseURL: "http://localhost:3344"
-});
+  baseURL: 'https://foodexplorer-api-g42c.onrender.com'
+  // baseURL: "http://localhost:3344"
+})
